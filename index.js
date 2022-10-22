aa4c391019794f86131c9feadbe4117fb85405c7
Raj <rajshinde2820@gmail.com>
2022-10-22 21:50:19 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function resume(){
     window.location.href="https://drive.google.com/file/d/1_SSPG2YMig4oLLk-WJih59G09mpSX92-/view?usp=sharing";
+   
 }
 function contact(){
     window.location.href="#footer";
