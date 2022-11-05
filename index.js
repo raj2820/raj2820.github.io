0aab6db2a1721e752f638e6905330d6b4e4170be
Raj_Shinde <rajshinde2820@gmail.com>
2022-11-05 19:36:34 +0530

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function resume(){
-    window.location.href="https://drive.google.com/u/2/uc?id=1_SSPG2YMig4oLLk-WJih59G09mpSX92-&export=download";
+// function resume(){
+//     window.location.href="https://drive.google.com/file/d/1_SSPG2YMig4oLLk-WJih59G09mpSX92-/view?usp=share_link";
    
-}
+// }
 function contact(){
     window.location.href="#footer";
 }
