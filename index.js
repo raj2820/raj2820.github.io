40b00f8b4e3cf0847c1dd69df386be71716f91d2
Raj_Shinde <rajshinde2820@gmail.com>
2022-12-10 22:25:58 +0530

    calendar

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 //     window.location.href="https://drive.google.com/file/d/1_SSPG2YMig4oLLk-WJih59G09mpSX92-/view?usp=share_link";
    
 // }
-function contact(){
-    window.location.href="#footer";
-}
+// function contact(){
+//     window.location.href="#footer";
+// }
